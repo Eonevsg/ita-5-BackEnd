9c16ded3dafbae1b44cc6de2f6e9028ffed84227
Eone <ivan.gorbun@stud.vgtu.lt>
2020-03-12 17:27:32 +0200

    Good start

---
+++ src/main/java/com/project/ita5/Ita5Application.java
@@ -0,0 +1,13 @@
+package com.project.ita5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ita5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ita5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ita5/Ita5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ita5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ita5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
