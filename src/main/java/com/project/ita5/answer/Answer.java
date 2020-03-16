fa4ebd12243ff3d7afb58633e5eb0ae3c54a1a65
Eone <ivan.gorbun@stud.vgtu.lt>
2020-03-16 10:19:37 +0200

    notation added

--- src/main/java/com/project/ita5/answer/Answer.java
+++ src/main/java/com/project/ita5/answer/Answer.java
@@ -24,7 +24,6 @@ public class Answer implements Serializable {
         return id;
     }
 
-
     public String getAnswer() {
         return answer;
     }
--- src/main/java/com/project/ita5/answer/AnswerRepository.java
+++ src/main/java/com/project/ita5/answer/AnswerRepository.java
@@ -2,8 +2,10 @@ package com.project.ita5.answer;
 
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AnswerRepository extends MongoRepository<Answer, String> {
 }
