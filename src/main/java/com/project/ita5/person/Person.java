555598a7e267520bbc63ca1717f6a7fe002ce6ab
Vyturys <vytur.jok@gmail.com>
2020-03-16 15:32:52 +0200

    Person validation

--- src/main/java/com/project/ita5/person/Person.java
+++ src/main/java/com/project/ita5/person/Person.java
@@ -3,7 +3,10 @@ package com.project.ita5.person;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class Person {
     @Transient
@@ -12,12 +15,19 @@ public class Person {
     @Id
     private String id;
     @NotBlank(message = "Name is required")
+    @Size(min = 1, max = 100, message="Name must be 1-100 characters long")
     private String name;
     @NotBlank(message = "Surname is required")
+    @Size(min = 1, max = 100, message="Surname must be 1-100 characters long")
     private String surname;
     @NotBlank(message = "Phone is required")
+    @Pattern(
+            regexp = "^\\+?[0-9]+$",
+            message = "Phone number should be vaild"
+    )
     private String phone;
     @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
     private String uni;
 
