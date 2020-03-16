9e812d10e55a996698a6fc75c5fe0f8318dd0c47
Vyturys <vytur.jok@gmail.com>
2020-03-16 18:57:07 +0200

    bandau auth

--- src/main/java/com/project/ita5/Ita5Application.java
+++ src/main/java/com/project/ita5/Ita5Application.java
@@ -3,9 +3,17 @@ package com.project.ita5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Ita5Application {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ita5Application.class, args);
--- src/main/java/com/project/ita5/user/UserController.java
+++ src/main/java/com/project/ita5/user/UserController.java
@@ -1,6 +1,7 @@
 package com.project.ita5.user;
 
 import com.project.ita5.authentication.Authentication;
+import com.project.ita5.database_sequence.SequenceGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Pair;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,21 +15,28 @@ public class UserController {
 
     UserRepository userRepository;
     Authentication authentication;
+    SequenceGeneratorService sequenceGeneratorService;
 
     @Autowired
-    public UserController(UserRepository userRepository, Authentication authentication){
+    public UserController(UserRepository userRepository, Authentication authentication, SequenceGeneratorService sequenceGeneratorService){
         this.userRepository = userRepository;
         this.authentication = authentication;
+        this.sequenceGeneratorService = sequenceGeneratorService;
     }
 
     @PostMapping("/log-in")
     public void logIn(@RequestBody User user) {
-
+        Pair<Boolean, String> temp = authentication.authenticate(user);
+        int asd = 5;
     }
 
     @PostMapping("/sign-up")
     public void registerNewUser(@RequestBody User user) {
-        Pair<Boolean, String> temp = authentication.authenticate(user);
-        int asd = 5;
+        User temp = userRepository.findByUsername(user.getUsername());
+        if (temp == null) {
+            user.setPassword(authentication.encode(user.getPassword()));
+            user.setId(Long.toString(sequenceGeneratorService.generateSequence(User.SEQUENCE_NAME)));
+            userRepository.save(user);
+        }
     }
 }
