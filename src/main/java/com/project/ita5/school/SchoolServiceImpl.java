e389e4dfaf714c971690aa63c8401f854f010b50
gaan3654 <g.anuzyte@yahoo.com>
2020-03-20 19:13:49 +0200

    Fetching a clean list of schools.

--- src/main/java/com/project/ita5/school/SchoolServiceImpl.java
+++ src/main/java/com/project/ita5/school/SchoolServiceImpl.java
@@ -4,11 +4,12 @@ import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.lang.reflect.Array;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class SchoolServiceImpl implements SchoolService {
@@ -17,12 +18,22 @@ public class SchoolServiceImpl implements SchoolService {
         RestTemplate restTemplate = new RestTemplate();
         String url1 = "https://lt.wikipedia.org/w/api.php?action=parse&format=json&prop=sections&page=Sąrašas:Lietuvos_aukštosios_mokyklos&section=1&prop=wikitext";
         String url2 = "https://lt.wikipedia.org/w/api.php?action=parse&format=json&prop=sections&page=Sąrašas:Lietuvos_aukštosios_mokyklos&section=4&prop=wikitext";
-        restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
+        restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
         Object result1 = restTemplate.getForObject(url1, Object.class);
         Object result2 = restTemplate.getForObject(url2, Object.class);
         List<Object> result = new ArrayList<>();
-        result.add(result1);
-        result.add(result2);
+        result.add(purifyText(result1));
+        result.add(purifyText(result2));
         return result;
+    }
+
+    private List<String> purifyText(Object input){
+        List<String> schools = new ArrayList<>();
+        Pattern pattern = Pattern.compile("(?<=\\[\\[)[A-Z-a-zą-žA-Ž ]*(?=\\]\\])");
+        Matcher matcher = pattern.matcher(input.toString());
+        while (matcher.find()) {
+            schools.add(matcher.group());
+        }
+        return schools;
     }
 }
