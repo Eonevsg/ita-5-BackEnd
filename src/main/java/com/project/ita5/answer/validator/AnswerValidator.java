e635bc655ec25bf29f23cc30690dbb16458bb926
Vyturys <vytur.jok@gmail.com>
2020-04-01 12:14:47 +0300

    Removed now unused variables

--- src/main/java/com/project/ita5/answer/validator/AnswerValidator.java
+++ src/main/java/com/project/ita5/answer/validator/AnswerValidator.java
@@ -8,9 +8,6 @@ import java.util.Arrays;
 
 public class AnswerValidator implements ConstraintValidator<Conditional, Answer> {
     private String message;
-    private String[] values250 = {"1", "2", "6"};
-    private String[] values450 = {"3", "4", "5"};
-    private String[] valuesYesNo = {"3", "4", "5", "6"};
 
     @Override
     public void initialize(Conditional constraintAnnotation) {
